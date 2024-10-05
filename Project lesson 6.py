540c4c06cf8352d2bf7dd2cee768baca197e5076
J450N-ong <odeleyejason@gmail.com>
2024-10-05 11:00:09 +0100

    Add files via upload

---
+++ Project lesson 6.py
@@ -0,0 +1,5 @@
+a="a"
+if a=="a"and"b"and"c"and"d"and"e"and"f"and"g"and"h"and"i"and"j"and"k"and"l"and"m"and"n"and"o"and"p"and"q"and"r"and"s"and"t"and"u"and"v"and"w"and"x"and"y"and"z":
+    print("a is an alphabet")
+else:
+    print("a is not an alphabet")
